22fb3ef8c2d316e36e37c341b635d42ed370ca3a
Daniel Richards <daniel.p.richards@gmail.com>
2015-08-31 17:04:18 +0100

    selected hex-tiles now animate

--- src/js/components/hex-tile.js
+++ src/js/components/hex-tile.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 import { Shape } from 'react-art';
+import { Spring } from 'react-motion';
 import { makeHexPath } from '../utils/hex-layout';
 
 export default class HexTile extends Component {
-  handleClick() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSelected: false
+    };
+  }
 
+  handleClick = () => {
+    this.setState({
+      isSelected: !this.state.isSelected
+    })
   }
 
   render() {
-    var color = '#111';
+    var color = '#888';
+    var opacity = 1;
+    var selectedSize = this.props.size;
+    var zIndex = 0;
+
+    if (this.state.isSelected) {
+      selectedSize = this.props.size * 0.6;
+      color = '#aaa';
+      zIndex = 1;
+    }
 
     // TODO - this could be optimised, don't need to calculate coords for every hex, just one and then offset.
-    var path = makeHexPath(this.props.size, this.props.centre);
-
     return (
-      <Shape d={path} fill={color} opacity='0.5' onClick={ this.handleClick }></Shape>
+      <Spring
+        defaultValue={ { val: this.props.size } }
+        endValue={ { val: selectedSize, config: [200, 15] } }>
+
+        { interpolated => (
+          <Shape
+            d={ makeHexPath(interpolated.val, this.props.centre) }
+            fill={ color }
+            opacity= { opacity }
+            onClick={ this.handleClick }
+            zIndex= { zIndex } /> )}
+
+      </Spring>
     );
   }
 }
